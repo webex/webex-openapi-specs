5b1c55b6d81aa7b174ea912b26160e07af275c79
arungane <arungane@cisco.com>
2025-07-21 10:46:17 -0400

    fix: remove mcp server

--- mcp/messaging mcp/Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:22.12-alpine AS builder
-
-WORKDIR /app
-COPY package.json package-lock.json ./
-RUN npm install
-
-COPY . .
-
-ENTRYPOINT ["node", "mcpServer.js"]
--- mcp/messaging mcp/index.js
+++
@@ -1,9 +0,0 @@
-import { Command } from "commander";
-import { registerToolsCommand } from "./commands/tools.js";
-
-const program = new Command();
-
-// Register commands
-registerToolsCommand(program);
-
-program.parse(process.argv);
